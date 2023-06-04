12fe96e689865e25672561f9cca0e8d0dd9b6605
unknown <dandisyahrullah.12@gmail.com>
2023-06-04 16:30:26 +0700

    test

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create base URL API
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "https://testing-production-718a.up.railway.app/api/v1",
 });
 
 // Set Authorization Token Header
